7f5d4b6996a05fdd417bb080c4bf8d6151919ff2
Ioan Popaluca <ioan.popaluca@gmail.com>
2026-02-24 20:29:17 +0200

    Refactor BETest

--- src/test/java/tests/UserLoginBETest.java
+++ src/test/java/tests/UserLoginBETest.java
@@ -7,89 +7,52 @@ import models.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import services.UserService;
 import sharedData.SharedData;
 
 public class UserLoginBETest extends SharedData {
     @Test
     public void userTest(){
-        RestAssured.baseURI="https://api.practicesoftwaretesting.com/";
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-type", "application/json");
-        request.header("Accept", "application/json");
+
 
         //pasul 1 : creem un nou user
-        System.out.println("STEP 1: CREATE USER REQUEST");
 
         AddressModel addressModel = new AddressModel("Street 1","City","State","Country","1234AA");
         RequestUserModel requestBody = new RequestUserModel("Ioan", "Popaluca",addressModel,"0987654321","1985-02-08","etwQu_@123","aapi@test.com");
 
-        request.body(requestBody);
-        Response response = request.post("/users/register");
-        System.out.println(response.getStatusLine());
-        response.body().prettyPrint();
-        ResponseUserModel responseBody = response.getBody().as(ResponseUserModel.class);
-        Assert.assertEquals(response.getStatusCode(), 201);
+        UserService userService = new UserService();
+       ResponseUserModel responseBody = userService.createUser(requestBody);
 
 
-        LoginPage loginPage = new LoginPage(getDriver());
-        loginPage.loginProcess(requestBody.getEmail(),requestBody.getPassword());
 
         //pasul 2:ne logam cu userul creat
-        System.out.println("STEP 2: LOGIN USER REQUEST ");
 
-        RequestUserLoginModel requestBody2 = new RequestUserLoginModel(requestBody.getEmail(),requestBody.getPassword());
-        request.body(requestBody2);
-        Response response2 = request.post("users/login");
-        response2.body().prettyPrint();
-        ResponseUserLoginModel responseBody2 = response2.getBody().as(ResponseUserLoginModel.class);
-        Assert.assertEquals(response2.getStatusCode(), 200);
+        ResponseUserLoginModel responseLoginBody= userService.loginUser(requestBody);
 
-        // verificam ca s-a creat userul
-        System.out.println("STEP 3:CHECK USER REQUEST");
+        //pasu 3  verificam ca s-a creat userul
 
-        request.header("Authorization","Bearer" + responseBody2.getAccess_token());
-        Response response3 = request.get("users/"+responseBody.getId());
-        System.out.println(response3.getStatusLine());
-        response3.body().prettyPrint();
-        Assert.assertEquals(response3.getStatusCode(),200);
 
-        System.out.println("STEP 4:LOGOUT USER REQUEST");
+        userService.checkUser(responseLoginBody.getAccess_token(),responseBody.getId(),200);
 
-        request.header("Authorization","Bearer" + responseBody2.getAccess_token());
-        Response response4 = request.get("users/logout");
-        System.out.println(response4.getStatusLine());
-        response4.body().prettyPrint();
-        Assert.assertEquals(response4.getStatusCode(),200);
+     //pasu 4 delogam userul
+
+        userService.logoutUser(responseLoginBody.getAccess_token());
 
         //pasul 5:ne logam cu ADMIN creat
-        System.out.println("STEP 5: LOGIN USER ADMIN REQUEST ");
 
-        RequestUserLoginModel requestBody5 = new RequestUserLoginModel("admin@practicesoftwaretesting.com","welcome01");
-        request.body(requestBody5);
-        Response response5 = request.post("users/login");
-        response5.body().prettyPrint();
-        ResponseUserLoginModel responseBody5 = response5.getBody().as(ResponseUserLoginModel.class);
-        Assert.assertEquals(response5.getStatusCode(), 200);
+        RequestUserLoginModel requestAdminBody = new RequestUserLoginModel("admin@practicesoftwaretesting.com","welcome01");
+
+        ResponseUserLoginModel responseAdminBody= userService.loginUser(requestAdminBody);
+
+        // Pasul 6 stergem userul
 
 
-
-        // verificam ca s-a creat userul
-        System.out.println("STEP 6:DELETE USER REQUEST");
-
-        request.header("Authorization","Bearer" + responseBody5.getAccess_token());
-        Response response6 = request.delete("users/"+responseBody.getId());
-        System.out.println(response6.getStatusLine());
-        response6.body().prettyPrint();
-        Assert.assertEquals(response6.getStatusCode(),204);
+        userService.deleteUser(responseAdminBody.getAccess_token(), responseBody.getId());
 
 
         // verificam ca  userul s-a sters
-        System.out.println("STEP 7:CHECK USER REQUEST");
 
-        request.header("Authorization","Bearer" + responseBody5.getAccess_token());
-        Response response7 = request.get("users/"+responseBody.getId());
-        System.out.println(response7.getStatusLine());
-        response7.body().prettyPrint();
-        Assert.assertEquals(response7.getStatusCode(),404);
+        userService.checkUser(responseLoginBody.getAccess_token(),responseBody.getId(),404);
+
     }
 }
